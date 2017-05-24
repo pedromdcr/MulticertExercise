61c2e5fac0c7c428c68b551ea1670f338c7b759c
pedromdcr <pedromdcrodrigues@gmail.com>
2017-05-24 19:56:54 +0100

    Created a sample web service. Working on project structure.

---
+++ src/main/java/com/pedromdcr/multicert/exercise/ClientManager.java
@@ -0,0 +1,17 @@
+package com.pedromdcr.multicert.exercise;
+
+public class ClientManager {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public float addValue(float value) {
+		return (value + 10);
+	}
+ 
+	public float subtractValue(float value) {
+		return (value - 10);
+	}
+	
+}
